c183ab20146678bf8cfc05d14d5c7039ba479bce
lmvb <luizmig52@gmail.com>
2023-09-25 16:47:40 -0300

    commit inicial

---
+++ src/main/java/br/com/microsservice/produtoms/ProdutoMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.microsservice.produtoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/microsservice/produtoms/ProdutoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microsservice.produtoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
